ddba7434dfc12d0ce2d86b4f440490f613861703
Bartłomiej Biernat <123biernat123@gmail.com>
2021-05-12 10:24:17 +0200

    Revert "redux almost finished - time for front"
    This reverts commit 3d85929243de2a7347c71f498dcab2094ca5b149.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,7 @@
 import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Nav from "./components/Nav"
-import FavouritePage from "./views/FavouritePage"
-import MainPage from "./views/MainPage"
-// youtube
-//  https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails%2C%20snippet%2C%20statistics&id=vJ3a_AuEW18&key=[YOUR_API_KEY]
-
-// VIMEO
-// https://api.vimeo.com/videos/181696349?access_token=[api_key]
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Nav />
-        <Switch>
-          <Route path="/" exact component={MainPage} />
-          <Route path="/favourite" exact component={FavouritePage} />
-        </Switch>
-      </Router>
-    </div>
-  )
+  return <div className="App"></div>
 }
 
 export default App
--- my-app/src/views/FavouritePage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function FavouritePage() {
-  return <div> Favourite Movies </div>
-}
-
-export default FavouritePage
